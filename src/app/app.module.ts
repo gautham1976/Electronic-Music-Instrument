a571cadb28f3cd6051edc5be958c2e087fc17926
gautham1976 <gautham1976@gmail.com>
2022-07-27 08:06:08 +0530

    3 pages created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgotpassComponent } from './forgotpass/forgotpass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignupComponent,
+    ForgotpassComponent
   ],
   imports: [
     BrowserModule,
