5bbf1782215ebac871a998583527b243424291c4
gautham1976 <gautham1976@gmail.com>
2022-11-03 08:55:16 +0530

    finished project

--- src/app/drums/drums.component.ts
+++ src/app/drums/drums.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drums.component.css']
 })
 export class DrumsComponent implements OnInit {
+  signdata={
+    signusername:"",
+    signemail:""
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {(() => {
+    'use strict'
+  
+    const forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms).forEach(form => {
+      form.addEventListener('submit',function(event:any){
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
   }
 
 }
